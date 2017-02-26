8f01e9d10d16640e5a55db1471257e5e4e0ab210
Markos Fragkakis <markos.fragkakis@gmail.com>
2017-02-26 23:13:41 +0200

    Autoclosing Document

--- src/main/java/org/marksoft/TableExtractor.java
+++ src/main/java/org/marksoft/TableExtractor.java
@@ -6,20 +6,22 @@ import com.snowtide.pdf.Page;
 import com.snowtide.pdf.layout.Table;
 import com.snowtide.pdf.util.TableUtils;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public class TableExtractor {
 
-    public void extractTables(InputStream pdfIs) {
-        Document pdf = PDF.open(pdfIs, "tables.pdf");
-        for(Page page : pdf.getPages()) {
-            List<Table> tables = TableUtils.getAllTables(page);
-            for(Table table : tables) {
-                String tableCsv = TableUtils.convertToCSV(table, ',');
-                System.out.println(tableCsv);
-            }
+    public void extractTables(InputStream pdfIs) throws IOException {
+        try (Document pdf = PDF.open(pdfIs, "foo")) {
+            for (Page page : pdf.getPages()) {
+                List<Table> tables = TableUtils.getAllTables(page);
+                for (Table table : tables) {
+                    String tableCsv = TableUtils.convertToCSV(table, ',');
+                    System.out.println(tableCsv);
+                }
 
+            }
         }
     }
 
